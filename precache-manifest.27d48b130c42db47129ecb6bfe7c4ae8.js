e1073221fe5193f7410266cd8e7585c812b32edc
sakshigoud <sakshigoud44@gmail.com>
2024-09-22 17:39:17 +0530

    Updates

---
+++ precache-manifest.27d48b130c42db47129ecb6bfe7c4ae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027c765c30233d89cc6544b586b20790",
+    "url": "/reacttuto/index.html"
+  },
+  {
+    "revision": "c2b7f508710cbb783377",
+    "url": "/reacttuto/static/css/main.d7dff318.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/reacttuto/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/reacttuto/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "c2b7f508710cbb783377",
+    "url": "/reacttuto/static/js/main.166ddd70.chunk.js"
+  },
+  {
+    "revision": "d17f82b8cd9d130a14dc",
+    "url": "/reacttuto/static/js/runtime~main.41ca7391.js"
+  }
+]);
